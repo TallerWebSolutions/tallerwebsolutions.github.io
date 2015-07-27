98bbc4d031c5cde00c810571bddaf45df0e0e216
Renato Vasconcellos <revagomes@gmail.com>
2015-07-26 22:52:38 -0300

    Improv. And CSS.

--- gulp/build.js
+++ gulp/build.js
@@ -1,10 +1,15 @@
 
 var gulp = require('gulp')
-  , sass = require('gulp-sass');
+  , sass = require('gulp-sass')
+  , autoprefixer = require('gulp-autoprefixer');
 
 gulp.task('sass', function () {
   return gulp.src('./sass/main.sass')
     .pipe(sass.sync().on('error', sass.logError))
+    .pipe(autoprefixer({
+      browsers: ['last 2 versions'],
+      cascade: false
+    }))
     .pipe(gulp.dest('./css'));
 });
 
