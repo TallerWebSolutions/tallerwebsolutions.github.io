62ebb2630b79a61a52cc71b8a24c2bcaf47fee62
Renato Vasconcellos <revagomes@gmail.com>
2015-07-27 00:47:44 -0300

    Diogo was asking for the code.

--- js/main.js
+++ js/main.js
@@ -1,7 +1,11 @@
-jQuery(document).ready(function() {
-  
-  //Hero
-  jQuery('#hero').height(jQuery(window).height());
-  
-  
-})
+
+
+$(function () {
+
+  // Instantiate smooth scroll on all links.
+  if ($.fn.smoothScroll) $('a[href^="#"]').smoothScroll({
+    easing: 'swing'
+  });
+
+});
+
