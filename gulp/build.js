f812a2d46cc8b65a764ff80c12d040f5b1d3e1ee
Lucas Constantino Silva <lucasconstantinosilva@gmail.com>
2015-07-31 03:06:32 -0300

    Created a clean-up task.

--- gulp/build.js
+++ gulp/build.js
@@ -1,8 +1,27 @@
 
 var gulp = require('gulp')
+  , gutil = require('gulp-util')
   , sass = require('gulp-sass')
-  , autoprefixer = require('gulp-autoprefixer');
+  , rimraf = require('rimraf')
+  , autoprefixer = require('gulp-autoprefixer')
 
+  , tmpDirBase = '.tmp'
+  , tmpDir = function (dir) { return tmpDirBase + (dir ? '/' + dir : ''); };
+
+/**
+ * Clean-up task.
+ * @todo : should also remove & checkout dist.
+ */
+gulp.task('clean', function (done) {
+  rimraf(tmpDir(), function () {
+    gutil.log('Removing', gutil.colors.cyan('\'' + tmpDir() + '\''));
+    done();
+  });
+});
+
+/**
+ * Compile Sass.
+ */
 gulp.task('sass', function () {
   return gulp.src('./sass/main.sass')
     .pipe(sass.sync().on('error', sass.logError))
