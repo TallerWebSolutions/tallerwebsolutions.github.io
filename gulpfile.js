02dc6d335a2dcac4407589e03823663f8f6f9a5d
Lucas Constantino Silva <lucasconstantinosilva@gmail.com>
2015-08-03 00:19:53 -0300

    Minor gulpfile comment.

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,12 @@
-/**
- * @file Main gulp file.
- */
+/*
+  gulpfile.js
+  ===========
+  Rather than manage one giant configuration file responsible
+  for creating multiple tasks, each task has been broken out into
+  its own file inside gulp directory. Any files in that directory
+  get automatically required below.
+  To add a new task, simply add a new task file that directory..
+*/
 
 // Load all tasks.
 require('require-dir')('./gulp', { recurse: true });
