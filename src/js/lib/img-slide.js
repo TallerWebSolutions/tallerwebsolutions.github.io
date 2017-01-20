ee1eb318b9edb42f9b0e7675527d0c7d4cb5e840
Rafael Caceres <rafael@taller.net.br>
2017-01-20 20:23:17 -0200

    Added new who section

--- src/js/lib/img-slide.js
+++ src/js/lib/img-slide.js
@@ -3,10 +3,17 @@ var $window = jQuery(window);
 
 jQuery(function () {
   var $container = jQuery('.img-slide');
+  var $servicesStack = jQuery('.services-stack');
 
   $container.on('inview', function(event, isInView) {
     if (isInView) {
       jQuery(this).addClass('img-slide-visible');
+    }
+  });
+
+  $servicesStack.on('inview', function(event, isInView) {
+    if (isInView) {
+      jQuery(this).addClass('rotate');
     }
   });
 });
