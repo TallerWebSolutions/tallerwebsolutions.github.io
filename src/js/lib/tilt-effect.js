b2daac79c52b375df1052a784b04fa44c78e5fc5
Sebastian Ferrari <sebas@taller.net.br>
2017-05-13 01:36:48 -0300

    Added tilt.js which use jQuery and fixed bug in Safari

--- src/js/lib/tilt-effect.js
+++ src/js/lib/tilt-effect.js
@@ -1,19 +1,9 @@
-var jQuery = require('jquery');
-var tilt = require('vanilla-tilt');
+var jQuery = require('jquery')
+var tiltjs = require('tilt.js')(jQuery)
 
-var $window = jQuery(window);
-
-jQuery(function () {
-  var $container = jQuery('#section-members ul li');
-
-  $container.on('inview', function(event, isInView) {
-    if (isInView) {
-      tilt.init(this, {
-        scale: 1.1,
-        max: 20
-      })
-    }
-  });
-});
-
-require('jquery-inview');
+var $container = tiltjs('#section-members ul li div').tilt({
+  glare: true,
+  maxGlare: .4,
+  scale: 1.1,
+  max: 10,
+})
