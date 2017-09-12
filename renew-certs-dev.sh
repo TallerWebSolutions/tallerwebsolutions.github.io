3395077e99911f643076e195b4233fff36aba116
Renato Vasconcellos <revagomes@gmail.com>
2017-09-12 14:53:36 -0300

    Added renew SSL certs script.

---
+++ renew-certs-dev.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -e
+
+# Renew certs.
+certbot renew
+
+# Copy blog certs.
+cp /etc/letsencrypt/live/taller-blog.dropit.in/fullchain.pem \
+  /home/taller-docker/docker-proxy/certs/taller-blog.dropit.in.crt
+cp /etc/letsencrypt/live/taller-blog.dropit.in/privkey.pem \
+  /home/taller-docker/docker-proxy/certs/taller-blog.dropit.in.key
+  
+# Copy site certs.
+cp /etc/letsencrypt/live/taller-site.dropit.in/fullchain.pem \
+  /home/taller-docker/docker-proxy/certs/taller-site.dropit.in.crt
+cp /etc/letsencrypt/live/taller-site.dropit.in/privkey.pem \
+  /home/taller-docker/docker-proxy/certs/taller-site.dropit.in.key
