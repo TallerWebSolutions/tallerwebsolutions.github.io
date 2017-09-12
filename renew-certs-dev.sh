213a00e4ead5b8276bc396219dfdf868a5b9b61b
Renato Vasconcellos <revagomes@gmail.com>
2017-09-12 15:51:08 -0300

    Updated renew-certs script to add  --quite and --noninteractive options.

--- renew-certs-dev.sh
+++ renew-certs-dev.sh
@@ -3,14 +3,14 @@
 set -e
 
 # Renew certs.
-certbot renew
+certbot renew --quite --noninteractive
 
 # Copy blog certs.
 cp /etc/letsencrypt/live/taller-blog.dropit.in/fullchain.pem \
   /home/taller-docker/docker-proxy/certs/taller-blog.dropit.in.crt
 cp /etc/letsencrypt/live/taller-blog.dropit.in/privkey.pem \
   /home/taller-docker/docker-proxy/certs/taller-blog.dropit.in.key
-  
+
 # Copy site certs.
 cp /etc/letsencrypt/live/taller-site.dropit.in/fullchain.pem \
   /home/taller-docker/docker-proxy/certs/taller-site.dropit.in.crt
